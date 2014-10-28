c24c0fafa10b81ef779ea0746fa34480236aff48
Matthew Solórzano <msolorzano768@gmail.com>
2014-10-28 15:35:28 -0400

    Fixed Accel
    again

--- SpaceGame/SpaceGame/classes/Player.cs
+++ SpaceGame/SpaceGame/classes/Player.cs
@@ -68,8 +68,8 @@ namespace SpaceGame
 
         private void calcAcceleration(GameTime gameTime)
         {
-            playerAcceleration.X = playerAcceleration.X + playerThrust.X;
-            playerAcceleration.Y = playerAcceleration.Y + playerThrust.Y;
+            playerAcceleration.X = playerThrust.X; //add gravity effect here
+            playerAcceleration.Y = playerThrust.Y; //add gravity effect here
 
             playerVelocity.X = playerAcceleration.X + playerVelocity.X;
             playerVelocity.Y = playerAcceleration.Y + playerVelocity.Y;
